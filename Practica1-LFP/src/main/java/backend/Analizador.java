5b33c9339e2ac9e26f21496f76c72b186a6e8961
OscarDeLeon19 <oscar99deleon19@gmail.com>
2021-08-03 20:21:36 -0600

    Se crea la comprobacion de un cadena de texto, ademas de la comprobacion de simbolos

--- Practica1-LFP/src/main/java/backend/Analizador.java
+++ Practica1-LFP/src/main/java/backend/Analizador.java
@@ -18,33 +18,118 @@ public class Analizador {
     }
 
     public void analizar(String texto, JTextArea AreaResultado, JTextArea AreaHistorial) {
+
+        System.out.println(texto);
         ArrayList<Integer> tokens = new ArrayList<Integer>();
         tokens.add(0);
         int x = 0;
         for (int i = 0; i < texto.length(); i++) {
             if (" ".equals(texto.substring(i, i + 1))) {
-                if (" ".equals(texto.substring(i + 1, i + 2))) {
+                if (i + 1 != texto.length()) {
+                    if (" ".equals(texto.substring(i + 1, i + 2))) {
 
-                } else {
-                    tokens.add(i);
-                    tokens.add(i + 1);
-                    x++;
+                    } else {
+
+                        tokens.add(i);
+                        tokens.add(i + 1);
+                        x++;
+                    }
                 }
-
             }
         }
         tokens.add(texto.length());
-        for (int i = 0; i < tokens.size(); i++) {
-            System.out.println(tokens.get(i));
-        }
         ArrayList<String> palabras = new ArrayList<String>();
         for (int i = 0; i < tokens.size(); i++) {
             palabras.add(texto.substring(tokens.get(i), tokens.get(i + 1)));
             i++;
         }
+
         for (int i = 0; i < palabras.size(); i++) {
-            System.out.println(palabras.get(i));
+            String palabra = palabras.get(i);
+            char caracteres[] = palabra.toCharArray();
+            if (Character.isLetter(caracteres[0])) {
+                boolean prueba = probarCadena(caracteres);
+                if (prueba == true) {
+                    System.out.println("Cadena");
+                } else {
+                    System.out.println("No cadena");
+                }
+            }
+
         }
     }
 
+    public boolean probarCadena(char[] caracteres) {
+        boolean comprobacion = true;
+        for (int i = 0; i < caracteres.length; i++) {
+            if (comprobarSimboloNoValido(caracteres[i]) == true) {
+                comprobacion = false;
+            }
+            if (comprobarSimboloNoValido(caracteres[i]) == true) {
+                comprobacion = false;
+            }
+        }
+        return comprobacion;
+    }
+
+    public boolean comprobarSimboloNoValido(char c) {
+        boolean comprobacion = false;
+        if (c == '!') {
+            comprobacion = true;
+        } else if (c == '#') {
+            comprobacion = true;
+        } else if (c == '$') {
+            comprobacion = true;
+        } else if (c == '%') {
+            comprobacion = true;
+        } else if (c == '&') {
+            comprobacion = true;
+        } else if (c == '/') {
+            comprobacion = true;
+        } else if (c == '=') {
+            comprobacion = true;
+        } else if (c == '?') {
+            comprobacion = true;
+        } else if (c == '¡') {
+            comprobacion = true;
+        } else if (c == '¿') {
+            comprobacion = true;
+        } else if (c == '+') {
+            comprobacion = true;
+        } else if (c == '-') {
+            comprobacion = true;
+        } else if (c == '*') {
+            comprobacion = true;
+        } else if (c == '>') {
+            comprobacion = true;
+        } else if (c == '<') {
+            comprobacion = true;
+        } else if (c == '@') {
+            comprobacion = true;
+        } else if (c == ':') {
+            comprobacion = true;
+        }
+        return comprobacion;
+    }
+
+    public boolean comprobarSimboloValido(char c) {
+        boolean comprobacion = false;
+        if (c == '[') {
+            comprobacion = true;
+        } else if (c == ']') {
+            comprobacion = true;
+        } else if (c == '{') {
+            comprobacion = true;
+        } else if (c == '}') {
+            comprobacion = true;
+        } else if (c == ';') {
+            comprobacion = true;
+        } else if (c == ',') {
+            comprobacion = true;
+        } else if (c == '.') {
+            comprobacion = true;
+            return comprobacion;
+        }
+        return comprobacion;
+    }
 }
