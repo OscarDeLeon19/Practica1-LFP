ad8ea1dd04e8f73bc6fff70639003deb783d5a2a
OscarDeLeon19 <oscar99deleon19@gmail.com>
2021-08-04 22:58:58 -0600

    Agregamos un metodo para mejorar las cadenas de texto y quitarle los espacios

--- Practica1-LFP/src/main/java/backend/Analizador.java
+++ Practica1-LFP/src/main/java/backend/Analizador.java
@@ -4,12 +4,14 @@ import java.util.ArrayList;
 import javax.swing.JTextArea;
 
 public class Analizador {
+
     /**
      * Constructor de la clase Analizador
      */
     public Analizador() {
-
+        
     }
+
     /**
      * Enum con los diferentes tipos de tokens que se van a utilizar
      */
@@ -21,22 +23,26 @@ public class Analizador {
         ERROR,
         IGNORAR;
     }
+
     /**
-     * Metodo utilizado para analizar la cadena de texto proveniente de la ventana
+     * Metodo utilizado para analizar la cadena de texto proveniente de la
+     * ventana
+     *
      * @param texto El texto enviado
      * @param AreaResultado El area donde salen los resultados del analisis
-     * @param AreaHistorial El area en donde se guardan los resultados correctos del analisis anteriores.
+     * @param AreaHistorial El area en donde se guardan los resultados correctos
+     * del analisis anteriores.
      */
     public void analizar(String texto, JTextArea AreaResultado, JTextArea AreaHistorial) {
         AreaResultado.setText(null);
-        ArrayList<Integer> numeros = new ArrayList<Integer>();
+        ArrayList<Integer> numeros = new ArrayList<>();
         numeros.add(0);
         int x = 0;
         for (int i = 0; i < texto.length(); i++) {
             if (" ".equals(texto.substring(i, i + 1)) || "\n".equals(texto.substring(i, i + 1))) {
                 if (i + 1 != texto.length()) {
-                    if (" ".equals(texto.substring(i + 1, i + 2)) || "\n".equals(texto.substring(i+1, i + 2))) {
-
+                    if (" ".equals(texto.substring(i + 1, i + 2)) || "\n".equals(texto.substring(i + 1, i + 2))) {
+                        
                     } else {
                         numeros.add(i);
                         numeros.add(i + 1);
@@ -46,9 +52,11 @@ public class Analizador {
             }
         }
         numeros.add(texto.length());
-        ArrayList<String> palabras = new ArrayList<String>();
+        ArrayList<String> palabras = new ArrayList<>();
         for (int i = 0; i < numeros.size(); i++) {
-            palabras.add(texto.substring(numeros.get(i), numeros.get(i + 1)));
+            String nueva_palabra = texto.substring(numeros.get(i), numeros.get(i + 1));
+            String arreglo = arreglarCadena(nueva_palabra);
+            palabras.add(arreglo);
             i++;
         }
         Token tokens[] = new Token[palabras.size()];
@@ -96,8 +104,10 @@ public class Analizador {
             }
         }
     }
+
     /**
      * Este metodo analiza una cadena de texto para ver si esta es correcta
+     *
      * @param caracteres Los caracteres de la cadena de texto enviada
      * @return True si es una cadena de texto y False si la cadena tiene errores
      */
@@ -113,10 +123,13 @@ public class Analizador {
         }
         return comprobacion;
     }
+
     /**
      * Comprueba si un numero es decimal o entero y si no tiene errores.
+     *
      * @param caracteres Los caracteres de la cadena de digitos enviados
-     * @return Si los digitos son correctos retorna true, si hay errores retorna false
+     * @return Si los digitos son correctos retorna true, si hay errores retorna
+     * false
      */
     public int probarDigito(char[] caracteres) {
         int numero = 1;
@@ -137,9 +150,12 @@ public class Analizador {
         }
         return numero;
     }
+
     /**
      * Prueba si un numero decimal enviado es correcto
-     * @param inicio El punto de la cadena en donde se comienza a ver si es decimal
+     *
+     * @param inicio El punto de la cadena en donde se comienza a ver si es
+     * decimal
      * @param caracteres Los caracteres que se pondran a prueba
      * @return Si el numero decimal es correcto o erroneo
      */
@@ -158,8 +174,10 @@ public class Analizador {
         }
         return numero;
     }
+
     /**
      * Comprueba los simbolos no validos en el programa
+     *
      * @param c El caracter que se comparara
      * @return true si el caracter es un simbolo, y false si no es un simbolo
      */
@@ -208,8 +226,10 @@ public class Analizador {
         }
         return comprobacion;
     }
+
     /**
      * Comprueba si un caracter es un simbolo valido
+     *
      * @param c El caracter que se comprobara
      * @return True si el caracter es un simbolo y false si no lo es
      */
@@ -231,5 +251,39 @@ public class Analizador {
             comprobacion = true;
         }
         return comprobacion;
+    }
+    /**
+     * Metodo para quitar los espacios en blanco que pueda tener una palabra
+     * @param palabra La palabra que se mejorara
+     * @return La palabra mejorada
+     */
+    public String arreglarCadena(String palabra) {
+        String nueva_palabra = "";
+        boolean arreglo = false;
+        int inicio = 0;
+        int fin = 0;
+        for (int i = 0; i < palabra.length(); i++) {
+            if (" ".equals(palabra.substring(i, i + 1))) {
+                inicio = i + 1;
+                arreglo = true;
+            } else {
+                i = palabra.length();
+            }
+        }
+        String nuevo = palabra.substring(inicio, palabra.length());
+        char caracteres[] = nuevo.toCharArray();
+        for (int i = 0; i < caracteres.length; i++) {
+            if (Character.isWhitespace(caracteres[i])) {
+                fin = i;
+                arreglo = true;
+                i = caracteres.length;
+            }
+        }
+        if (arreglo == true) {
+            nueva_palabra = palabra.substring(inicio, inicio + fin);
+        } else {
+            nueva_palabra = palabra;
+        }
+        return nueva_palabra;
     }
 }
